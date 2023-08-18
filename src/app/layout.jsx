62d75ebb9198aa80037c1482ab922a922fa9f30a
FilipTheGod <filipstan666@gmail.com>
2023-08-18 16:06:18 +0300

    analytics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { Inter, Lexend } from 'next/font/google'
 import clsx from 'clsx'
+import { Analytics } from '@vercel/analytics/react';
 
 import '@/styles/tailwind.css'
 
@@ -34,7 +35,10 @@ export default function RootLayout({ children }) {
         lexend.variable
       )}
     >
-      <body className="flex h-full flex-col">{children}</body>
+      <body className="flex h-full flex-col">
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
